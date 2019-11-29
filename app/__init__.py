20b096a34f52d43a5693a13757dcaaa30dcdf52a
Charlie Splittstoser <split022@umn.edu>
2019-11-29 17:12:49 -0600

    Login and register working

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, Response, render_template
 from flask_login import LoginManager, UserMixin
-from flask_sqlalchemy import SQLAlchemy, inspect
+from flask_sqlalchemy import SQLAlchemy, inspect, relationship
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/TestDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "blah123"
 app.config['TESTING'] = False
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,30 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, flash, request, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from app.forms import LoginForm
+from app.models import User, Course
+from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
+
+
+@app.route('/initdb')
+def initdb():
+    db.create_all()
+    return "DB initialized"
 
 @app.route('/')
 @app.route('/index')
-#@login_required
+@login_required
 def index():
-    db.create_all()
     admin = User(first_name='bobby', last_name='clasemann', email='bob@example.com', person_key=1)
     admin2 = User(first_name='charlie', last_name='split', email='split@example.com', person_key=1)
     course_one = Course(course_id=4131, title='CSCI4131', credits=3)
 
     print("Hello")
 
-    db.session.add(admin)
-    db.session.add(admin2)
-    db.session.add(course_one)
-    db.session.commit()
+    #db.session.add(admin)
+    #db.session.add(admin2)
+    #db.session.add(course_one)
+    #db.session.commit()
 
     print(User.query.all())
 
@@ -28,9 +33,37 @@ def index():
     #return "Hello, World!"
     return render_template("index.html", title='Home Page')
 
-@app.route('/login')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data,
+                    email=form.email.data, person_key=0) #Default as student for now CHANGE LATER
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
